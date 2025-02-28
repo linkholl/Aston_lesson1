c1c00bc10a098c05d8e77532913635a8078a5360
linkholl <ohan073@gmail.com>
2025-03-01 00:28:58 +0300

    add function say my name

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-console.log("Hi world!")
 function sayHello() {
     console.log("Привет, мир!");
 }
 
+function sayGoodbye() {
+    console.log("Пока, мир!");
+}
+
+function sayName(name) {
+    console.log(`Меня зовут ${name}`);
+}
+
 sayHello();
+sayGoodbye();
+sayName("Habi");
